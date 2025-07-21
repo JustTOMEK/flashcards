1b750d8e88747bf7ed9819bfe508ff0b0d474da0
Tomaszek <trunio31@gmail.com>
2025-07-21 12:09:07 +0200

    Added a route for flashcard sets

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,7 @@ const PORT = 3000;
 app.use(cors())
 app.use(express.json());
 app.use('/flashcards', flashcardsRouter);
+app.use('/flashcardSets', flashcardsRouter);
 
 initDB().then(() => {
   app.listen(PORT, () => {
