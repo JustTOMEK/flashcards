32bb952c746cad6ff828b8b954fc95dfa73ec086
Tomaszek <trunio31@gmail.com>
2025-07-21 16:02:13 +0200

    Added routing for displaying flashcards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router";
-import FlashcardSets from './components/Sets'
+import { BrowserRouter, Routes, Route } from "react-router"
+import FlashcardSets from './components/FlashcardSets'
 import AddSet from './components/AddSet'
+import Flashcards from './components/Flashcards'
+import AddFlashcard from './components/AddFlashcard'
 
 function App() {
   return (
@@ -9,8 +11,14 @@ function App() {
       <div className = "App">
         <h1> APP HERE</h1>
           <Routes>
-            <Route path = "/" element = { <> <FlashcardSets />
-        <AddSet /> </>} />     
+            <Route path = "/" element = { 
+              <> <FlashcardSets />
+                  <AddSet />
+              </>} />
+            <Route path = "/set" element = {
+              <> <Flashcards />
+                  <AddFlashcard />
+              </>} />
           </Routes>
       </div>
     </BrowserRouter>
