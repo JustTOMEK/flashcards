d4c25253bad88822c7b032e008ffcf82604b0c0d
Tomaszek <trunio31@gmail.com>
2025-07-21 11:11:51 +0200

    Delete flashcard functional

--- frontend/src/components/Flashcards.tsx
+++ frontend/src/components/Flashcards.tsx
@@ -3,7 +3,10 @@ import React, {useEffect, useState} from 'react'
 type Flashcard = {
     word: string,
     translation: string
+    id: string
 }
+
+
 
 const Flashcards: React.FC = () => {
     const [flashcards, setFlashcards] = useState<Flashcard> ([])
@@ -16,6 +19,13 @@ const Flashcards: React.FC = () => {
 
     }, [])
 
+    const handleDelete = async (id: string) => {
+        fetch(`http://localhost:3000/flashcards/${id}`, {
+            method: 'DELETE',
+        });
+        setFlashcards((previous) => previous.filter((card) => card.id !== id));
+    };
+
     return (
         <div>
             <h1> Flashcards: </h1>
@@ -23,6 +33,7 @@ const Flashcards: React.FC = () => {
                 {flashcards.map((card, index) => (
                     <li key={index}>
                         <strong> {card.word} </strong> : {card.translation}
+                        <button onClick={ () => handleDelete(card.id)} > Id: {card.id} </button>
                     </li>
                 ))}
 
