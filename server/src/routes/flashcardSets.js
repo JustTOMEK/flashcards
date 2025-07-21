b9790fb35fd3d772d88c92c9473529adc39cb4ba
Tomaszek <trunio31@gmail.com>
2025-07-21 12:08:36 +0200

    Defined requests for flashcard sets

---
+++ server/src/routes/flashcardSets.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const {db} = require('../db/lowdb')
+const { v4: uuidv4 } = require('uuid');
+
+const router = express.Router()
+
+router.get('/', async (req, res ) => {
+    await db.read()
+    const flashcardSets = db.data?.flashcardSets || []
+    res.json(flashcardSets)
+})
+
+router.post('/', async (req, res) => {
+    const {description} = req.body
+
+    const id = uuidv4()
+
+    const newFlashcardSet = {id, description}
+
+    if (!db.data.flashcardSets) {
+        db.data.flashcardSets = [];
+    }
+
+    db.data.flashcards.push(newFlashcardSet)
+
+    res.status(201).json(newFlashcardSet)
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params
+    await db.read()
+
+    const flashcardSets = db.data?.flashcardSets || [];
+
+    const index = flashcardSets.findIndex(card => card.id === id)
+
+    flashcardSets.splice(index, 1);
+
+    const flashcards = db.data?.flashcards || [];
+
+    for (let i = flashcards.length - 1; i >=0; i--){
+        if (flashcards[i].id === id){
+            flashcards.splice(i, 1)
+        }
+    }
+
+    await db.write();
+
+    res.status(200).json(newFlashcard)  
+})
+
+module.exports = router
