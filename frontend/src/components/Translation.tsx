a253bd60108de17ec40dbb505a997ffd1f5f7ba5
Tomaszek <trunio31@gmail.com>
2025-07-22 11:33:49 +0200

    Configured and added libretranslate api

---
+++ frontend/src/components/Translation.tsx
@@ -0,0 +1,30 @@
+import '../App.css'
+
+const Translation: React.FC = () => {
+
+
+    const handleButtonclick = async () => {
+
+    const res = await fetch("http://127.0.0.1:5000/translate", {
+        method: "POST",
+        body: JSON.stringify({
+            q: "Witam psy!",
+            source: "pl",
+            target: "de"
+        }),
+        headers: {
+            "Content-Type": "application/json"}
+        });
+
+    console.log(await res.json());
+    }
+    
+    return (
+        <>
+            Jesteśmy Tutaj
+            <button className='px-4 bg-red-500 rounded' onClick={handleButtonclick} > Przycisk szefa </button>
+        </>
+    )
+}
+
+export default Translation
