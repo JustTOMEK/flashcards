8708782c7bf793dbec5293b058b36d5aa549c574
Tomaszek <trunio31@gmail.com>
2025-07-22 16:25:34 +0200

    Fixed user attribute issue

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const {db} = require('../db/lowdb')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
@@ -10,7 +11,7 @@ router.post('/', async (req, res) =>{
     const {username, password} = req.body
 
     const user = db.data.users.find(user => user.username === username)
-    const password_good = await bcrypt.compare(password, user.password)
+    const password_good = await bcrypt.compare(password, user.hashedPassword)
 
     if (!user || !password_good ){
         return res.status(401).json({message: 'Invalid username or password'})
@@ -19,6 +20,8 @@ router.post('/', async (req, res) =>{
     const JWT_SECRET = process.env.JWT_SECRET
 
     const token = jwt.sign({username: username}, JWT_SECRET, {expiresIn: '1h'})
+
+    return res.status(200).json({message: 'Login successful', token})
 })
 
 module.exports = router
