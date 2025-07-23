fae7652e59d7d39a0f1d1c4ff4f459e514b32b42
Tomaszek <trunio31@gmail.com>
2025-07-23 13:04:33 +0200

    Login now passes userId instead of username

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
         return res.status(401).json({ message: 'Invalid username or password' })
     }
 
-    const token = jwt.sign({ username: username }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
         expiresIn: '1h',
     })
 
