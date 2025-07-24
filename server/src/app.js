8474b4423c6d4f47690f2c0c87840da167624bc5
Tomaszek <trunio31@gmail.com>
2025-07-24 13:49:04 +0200

    split server.js into to files for testing

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const cors = require('cors')
+const express = require('express')
+const flashcardsRouter = require('./routes/flashcards')
+const flashcardSetsRouter = require('./routes/flashcardSets')
+const registerRouter = require('./routes/register')
+const loginRouter = require('./routes/login')
+const authenticateRouter = require('./routes/authenticate')
+const meRouter = require('./routes/me')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use('/flashcards', flashcardsRouter)
+app.use('/flashcardSets', flashcardSetsRouter)
+app.use('/register', registerRouter)
+app.use('/login', loginRouter)
+app.use('/authenticate', authenticateRouter)
+app.use('/me', meRouter)
+
+module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,10 @@
-const cors = require('cors')
-const express = require('express')
-const flashcardsRouter = require('./routes/flashcards')
-const flashcardSetsRouter = require('./routes/flashcardSets')
-const registerRouter = require('./routes/register')
-const loginRouter = require('./routes/login')
-const authenticateRouter = require('./routes/authenticate')
-const meRouter = require('./routes/me')
+const app = require('./app')
 const { initDB } = require('./db/lowdb')
 
-const app = express()
+
 const PORT = 3000
 
-app.use(cors())
-app.use(express.json())
-app.use('/flashcards', flashcardsRouter)
-app.use('/flashcardSets', flashcardSetsRouter)
-app.use('/register', registerRouter)
-app.use('/login', loginRouter)
-app.use('/authenticate', authenticateRouter)
-app.use('/me', meRouter)
+
 
 initDB()
     .then(() => {
