0c9d968fe54fe74d6e7b63a4ecdc59aa338d0d5d
Tomaszek <trunio31@gmail.com>
2025-07-28 15:50:20 +0200

    /me now return username alongside userId

--- server/src/routes/me.js
+++ server/src/routes/me.js
@@ -1,15 +1,20 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
+const { db } = require('../db/lowdb')
 const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
+    await db.read()
     const token = req.headers['token']
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         const userId = decoded.id
-        return res.status(200).json({ userId })
+        console.log(userId)
+        const user = db.data.users.find(u => u.id === userId)
+        console.log(user)
+        return res.status(200).json({ userId, username: user.username })
     } catch (error) {
         return res.status(401).json({ error: 'Invalid token' })
     }
