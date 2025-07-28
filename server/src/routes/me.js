fb6764ec2d170545e2fa95a87bf5d3186df7affd
Tomaszek <trunio31@gmail.com>
2025-07-28 15:52:34 +0200

    Deleted logs

--- server/src/routes/me.js
+++ server/src/routes/me.js
@@ -11,9 +11,7 @@ router.get('/', async (req, res) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
         const userId = decoded.id
-        console.log(userId)
         const user = db.data.users.find((u) => u.id === userId)
-        console.log(user)
         return res.status(200).json({ userId, username: user.username })
     } catch (error) {
         return res.status(401).json({ error: 'Invalid token' })
