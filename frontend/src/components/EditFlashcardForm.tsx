142b79fe2f0a14e06e7da075aa66f2d037b369b2
Tomaszek <trunio31@gmail.com>
2025-07-30 10:55:18 +0200

    Changed icons

--- frontend/src/components/EditFlashcardForm.tsx
+++ frontend/src/components/EditFlashcardForm.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import withAuth from "./withAuth";
 import { IoMdCloseCircleOutline } from "react-icons/io";
+import { MdTranslate } from "react-icons/md";
+import { FaGlobe } from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
+
+
 
 type Flashcard = {
     word: string
@@ -49,7 +54,7 @@ const EditFlashcardForm: React.FC<{
     }
 
     return (
-        <div className="relative h-1/4 w-1/4 bg-tan rounded-xl gap-4 p-7 text-cream grid md:grid-cols-2  grid-cols-1 whitespace-nowrap shadow-md hover:shadow-lg transition-shadow" >
+        <div className="overflow-auto relative h-1/3 w-1/3 bg-tan rounded-xl gap-4 p-7 text-cream grid md:grid-cols-2  grid-cols-1 whitespace-nowrap shadow-md hover:shadow-lg transition-shadow"  >
             
             <button
                             onClick={onExit}
@@ -76,14 +81,16 @@ const EditFlashcardForm: React.FC<{
             />
             <button
                 onClick={handleTranslate}
-                className="bg-olive px-4 py-2 rounded   items-center justify-center gap-2 "
+                className="bg-olive px-4 py-2 rounded flex  items-center justify-center gap-2 "
             >
+                <FaGlobe />
                 Translate
             </button>
             <button
                 onClick={handleEdit}
-                className="bg-olive px-4 py-2 rounded   items-center justify-center gap-2 "
+                className="bg-olive px-4 py-2 rounded  flex items-center justify-center gap-2 "
             >
+                <FaCheckCircle />
                 Confirm
             </button>
             
