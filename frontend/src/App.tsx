bcdc7497f716173c692b7bbdf1eae4389d5632c8
Tomaszek <trunio31@gmail.com>
2025-07-30 15:17:53 +0200

    Created AppContent to use location

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,12 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Flashcards from './components/Flashcards'
-import Translation from './components/Translation'
-import FlashcardSets from './components/FlashcardSets'
-import Login from './components/Login'
-import Register from './components/Register'
-import Practice from './components/Practice'
-import Welcome from './components/Welcome'
+import { BrowserRouter} from 'react-router'
+import AppContent from './AppContent'
+
 function App() {
+
     return (
         <BrowserRouter>
-            <div className="App">
-                <Routes>
-                    <Route path="/welcome" element={<Welcome />} />
-
-                    <Route path="/" element={<FlashcardSets />} />
-
-                    <Route path="/set" element={<Flashcards />} />
-
-                    <Route path="/translation" element={<Translation />} />
-
-                    <Route path="/login" element={<Login />} />
-
-                    <Route path="/register" element={<Register />} />
-
-                    <Route path="/practice" element={<Practice />} />
-                </Routes>
-            </div>
+            <AppContent />
         </BrowserRouter>
     )
 }
---
+++ frontend/src/AppContent.tsx
@@ -0,0 +1,37 @@
+import './App.css'
+import {Routes, Route, useLocation } from 'react-router'
+import Flashcards from './components/Flashcards'
+import Translation from './components/Translation'
+import FlashcardSets from './components/FlashcardSets'
+import Login from './components/Login'
+import Register from './components/Register'
+import Practice from './components/Practice'
+import Welcome from './components/Welcome'
+import Navbar from './components/Navbar'
+function AppContent() {
+    const location = useLocation();
+    const noNavbarPaths = ["/login", "/register", "/welcome"]
+    const shouldHideNavbar = noNavbarPaths.includes(location.pathname);
+    return (
+            <div className="App">
+                {!shouldHideNavbar && <Navbar />}
+                <Routes>
+                    <Route path="/welcome" element={<Welcome />} />
+
+                    <Route path="/" element={<FlashcardSets />} />
+
+                    <Route path="/set" element={<Flashcards />} />
+
+                    <Route path="/translation" element={<Translation />} />
+
+                    <Route path="/login" element={<Login />} />
+
+                    <Route path="/register" element={<Register />} />
+
+                    <Route path="/practice" element={<Practice />} />
+                </Routes>
+            </div>
+    )
+}
+
+export default AppContent
