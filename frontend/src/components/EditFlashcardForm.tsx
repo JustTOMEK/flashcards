851cf213b49fc2ec7e981ff78fdba2dade78a520
Tomaszek <trunio31@gmail.com>
2025-07-30 16:54:32 +0200

    Made navbar clicakble over forms

--- frontend/src/components/EditFlashcardForm.tsx
+++ frontend/src/components/EditFlashcardForm.tsx
@@ -40,10 +40,10 @@ const EditFlashcardForm: React.FC<{
     function handleTranslate(): void {}
 
     return (
-        <div className="overflow-auto relative h-1/3 w-1/3 bg-tan rounded-xl gap-4 p-7 text-cream grid md:grid-cols-2  grid-cols-1 whitespace-nowrap shadow-md hover:shadow-lg transition-shadow">
+        <div className="overflow-auto relative h-1/3 w-1/3 bg-olive rounded-xl gap-4 p-7 text-cream grid md:grid-cols-2  grid-cols-1 whitespace-nowrap shadow-md hover:shadow-lg transition-shadow">
             <button
                 onClick={onExit}
-                className="absolute top-1 right-1 text-olive text-xl font-bold"
+                className="absolute top-1 right-1 text-tan text-xl font-bold"
             >
                 <IoMdCloseCircleOutline className="text-3xl" />
             </button>
@@ -69,14 +69,14 @@ const EditFlashcardForm: React.FC<{
             />
             <button
                 onClick={handleTranslate}
-                className="bg-olive px-4 py-2 rounded flex  items-center justify-center gap-2 "
+                className="bg-tan px-4 py-2 rounded flex  items-center justify-center gap-2 "
             >
                 <FaGlobe />
                 Translate
             </button>
             <button
                 onClick={handleEdit}
-                className="bg-olive px-4 py-2 rounded  flex items-center justify-center gap-2 "
+                className="bg-tan px-4 py-2 rounded  flex items-center justify-center gap-2 "
             >
                 <FaCheckCircle />
                 Confirm
