0c2f235923fc14cfdd3cb2a7c313900057fc79ad
Tomaszek <trunio31@gmail.com>
2025-07-31 16:32:55 +0200

    added statistics page with 2 charts

---
+++ frontend/src/components/Statistics.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react";
+import { Bar, Doughnut } from "react-chartjs-2";
+import { FaFire } from "react-icons/fa";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
+} from "chart.js";
+import withAuth from "./withAuth";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(ArcElement);
+
+function Statistics() {
+    const [barResponse, setBarResponse] = useState([]);
+    const [doughnutResponse, setDougnutResponse] = useState([])
+
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/statistics/percentage`, {
+            headers: {
+                token: token ?? '',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setBarResponse(data);
+            });
+        fetch(`http://localhost:3000/statistics/completed`, {
+            headers: {
+                token: token ?? '',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setDougnutResponse(data);
+            });
+    }, []);
+
+    const labels = barResponse.map(item => item.setName);
+    const percentages = barResponse.map(item => parseFloat(item.level3Percentage.replace('%', '')));
+
+    const barData = {
+        labels,
+        datasets: [
+            {
+                label: "Percentage of learned flashcards",
+                data: percentages,
+                backgroundColor: "#bc6c25",
+                borderColor: "#dda15e",
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const totalRepetitions = doughnutResponse.repetitions
+    const completedCount = doughnutResponse.completed; // Replace with actual count
+    const notCompletedCount = doughnutResponse.notCompleted; // Replace with actual count
+    console.log(doughnutResponse)
+    const doughnutData = {
+        labels: ["Completed", "Not Completed"],
+        datasets: [
+            {
+                data: [completedCount, notCompletedCount],
+                backgroundColor: ["#606c38", "#bc6c25"],
+                borderColor: ["#283618", "#dda15e"],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+
+    return (
+        <>
+        <div className="p-4 text-center">
+            <h2 className="text-2xl font-semibold"> Practice Summary</h2>
+            <p className="text-lg mt-2">Total Practiced Cards: <span className="font-bold">{totalRepetitions}</span></p>
+
+            <p className="flex justify-center items-center gap-2">
+                Daily Streak: <span className="font-bold ml-1">5 days</span> <FaFire className="text-burnt-orange" />
+            </p>
+        </div>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+        <div className="p-4 h-[200px] md:h-[300px]">
+          <Bar data={barData} options={{ maintainAspectRatio: false }} />
+        </div>
+      
+        <div className="p-4 flex justify-center h-[200px] md:h-[300px]">
+          <Doughnut data={doughnutData} options={{ maintainAspectRatio: false }} />
+        </div>
+      </div>
+      </>
+      
+
+    );
+    
+}
+
+export default withAuth(Statistics);
