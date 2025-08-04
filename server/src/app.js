0cfa3f67c1ea5dbb855caebf8883bf41f30d8003
Tomaszek <trunio31@gmail.com>
2025-08-04 15:23:32 +0200

    refactored into two files to import app

---
+++ server/src/app.js
@@ -0,0 +1,32 @@
+const cors = require('cors')
+const express = require('express')
+const { createDB } = require('./db/lowdb')
+const { createRegisterRouter } = require('./routes/register')
+const { createLoginRouter } = require('./routes/login')
+const { createFlashcardsRouter } = require('./routes/flashcards')
+const { createFlashcardSetsRouter } = require('./routes/flashcardSets')
+const { createAuthenticateRouter } = require('./routes/authenticate')
+const { createMeRouter } = require('./routes/me')
+const { createStatisticsRouter } = require('./routes/statistics')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const initApp = async () => {
+  const { db, initDB } = createDB('json', 'db.json')
+  await initDB()
+
+  app.locals.db = db
+
+  app.use('/register', createRegisterRouter(db))
+  app.use('/login', createLoginRouter(db))
+  app.use('/flashcards', createFlashcardsRouter(db))
+  app.use('/flashcardSets', createFlashcardSetsRouter(db))
+  app.use('/authenticate', createAuthenticateRouter(db))
+  app.use('/me', createMeRouter(db))
+  app.use('/statistics', createStatisticsRouter(db))
+}
+
+module.exports = { app, initApp }
--- server/src/server.js
+++ server/src/server.js
@@ -1,36 +1,10 @@
-const cors = require('cors')
-const express = require('express')
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-const { createDB } = require('./db/lowdb')
-const {createRegisterRouter} = require('./routes/register')
-const { createLoginRouter } = require('./routes/login')
-const { createFlashcardsRouter } = require('./routes/flashcards')
-const { createFlashcardSetsRouter } = require('./routes/flashcardSets')
-const { createAuthenticateRouter } = require('./routes/authenticate')
-const { createMeRouter } = require('./routes/me')
-const { createStatisticsRouter } = require('./routes/statistics')
+const { app, initApp } = require('./app')
 
 const PORT = 3000
 
 const startServer = async () => {
   try {
-    const { db, initDB } = createDB('json', 'db.json')
-    await initDB()
-
-    app.locals.db = db 
-
-    app.use('/register', createRegisterRouter(db))
-    app.use('/login', createLoginRouter(db))
-    app.use('/flashcards', createFlashcardsRouter(db))
-    app.use('/flashcardSets', createFlashcardSetsRouter(db))
-    app.use('/authenticate', createAuthenticateRouter(db))
-    app.use('/me', createMeRouter(db))
-    app.use('/statistics', createStatisticsRouter(db))
-
+    await initApp()
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`)
     })
