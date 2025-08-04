7db3891f09bff2f160cf16983ef7aaa8e189076d
Tomaszek <trunio31@gmail.com>
2025-08-04 15:46:33 +0200

    tests for login with new setup

--- server/src/tests/login.test.js
+++ server/src/tests/login.test.js
@@ -1,68 +1,81 @@
 import { test, expect, beforeEach } from 'vitest'
-const request = require('supertest')
-import app from '../app'
-import { db } from '../db/lowdb'
-const bcrypt = require('bcrypt')
+import request from 'supertest'
+import { app, initApp } from '../app'
+import { createDB } from '../db/lowdb'
+import bcrypt from 'bcrypt'
 
 process.env.JWT_SECRET = 'test'
 
-beforeEach(async () => {
-    const hashedPassword = await bcrypt.hash('passworddd', 10)
+let testDb;
 
-    db.data = {
-        users: [
-            {
-                id: 'user1',
-                hashedPassword,
-                username: 'janek',
-            },
-        ],
-    }
-    await db.write()
-})
+beforeEach(async () => {
+  const { db, initDB } = createDB('memory');
+  testDb = db;
+  await initDB();
+
+  const hashedPassword = await bcrypt.hash('passworddd', 10);
+
+  testDb.data = {
+    users: [
+      {
+        id: 'user1',
+        hashedPassword,
+        username: 'janek',
+      },
+    ],
+    flashcardSets: [],
+    flashcards: [],
+  };
+
+  await testDb.write();
+
+  app.locals.db = testDb;
+
+  await initApp(testDb);
+});
 
 test('Successful login', async () => {
-    const res = await request(app).post('/login').send({
-        username: 'janek',
-        password: 'passworddd',
-    })
-    expect(res.status).toBe(200)
-    expect(res.body).toHaveProperty('token')
-    expect(res.body.message).toBe('Login successful')
-})
+  const res = await request(app).post('/login').send({
+    username: 'janek',
+    password: 'passworddd',
+  });
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty('token');
+  expect(res.body.message).toBe('Login successful');
+});
 
-test('wrong password', async () => {
-    const res = await request(app).post('/login').send({
-        username: 'janek',
-        password: 'passwordd',
-    })
-    expect(res.status).toBe(401)
-    expect(res.body.message).toBe('Invalid password')
-})
+test('Wrong password', async () => {
+  const res = await request(app).post('/login').send({
+    username: 'janek',
+    password: 'passwordd',
+  });
+  expect(res.status).toBe(401);
+  expect(res.body.message).toBe('Invalid password');
+});
 
-test('Not exisiting user', async () => {
-    const res = await request(app).post('/login').send({
-        username: 'tomek',
-        password: 'passwordd',
-    })
-    expect(res.status).toBe(401)
-    expect(res.body.message).toBe('Invalid username')
-})
+test('Non-existing user', async () => {
+  const res = await request(app).post('/login').send({
+    username: 'tomek',
+    password: 'passwordd',
+  });
+  expect(res.status).toBe(401);
+  expect(res.body.message).toBe('Invalid username');
+});
 
 test('Empty password', async () => {
-    const res = await request(app).post('/login').send({
-        username: 'tomek',
-        password: '',
-    })
-    expect(res.status).toBe(400)
-    expect(res.body.message).toBe('Username and password cannot be empty')
-})
+  const res = await request(app).post('/login').send({
+    username: 'tomek',
+    password: '',
+  });
+  expect(res.status).toBe(400);
+  expect(res.body.message).toBe('Username and password cannot be empty');
+});
 
-test('Empty user', async () => {
-    const res = await request(app).post('/login').send({
-        username: '',
-        password: 'passwordd',
-    })
-    expect(res.status).toBe(400)
-    expect(res.body.message).toBe('Username and password cannot be empty')
-})
+test('Empty username', async () => {
+  const res = await request(app).post('/login').send({
+    username: '',
+    password: 'passwordd',
+  });
+  expect(res.status).toBe(400);
+  expect(res.body.message).toBe('Username and password cannot be empty');
+});
