a1f85ed0a03e758828dad497910388e3ac89ccb6
Tomaszek <trunio31@gmail.com>
2025-08-06 10:27:46 +0200

    Created a home icon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import withAuth from './withAuth';
-import { FaRegUserCircle } from 'react-icons/fa';
-import { IoStatsChart, IoLogOut } from 'react-icons/io5';
+import { FaRegUserCircle, FaHome } from 'react-icons/fa';
+import { IoStatsChart } from 'react-icons/io5';
 import { useNavigate } from 'react-router';
 
 function Navbar() {
@@ -14,46 +14,50 @@ function Navbar() {
 
   return (
     <div className="sticky top-0 bg-olive text-white px-4 py-3 flex justify-between items-center z-[2116]">
-      <p className="text-2xl">Welcome debilu to Flashcards</p>
+      <p className="text-2xl"> Flashcards</p>
+        <div className="text-3xl flex justify-center items-center gap-3 relative">
 
-      <div className="text-3xl flex justify-center items-center gap-3 relative">
-        <IoStatsChart
-          aria-label="Statistics"
-          onClick={() => navigate('/statistics')}
-          className="cursor-pointer"
-        />
+        <FaHome
+            aria-label="Home"
+            onClick={() => navigate('/')}
+            className="cursor-pointer"
+          />
 
-        {/* Profile Icon and Dropdown */}
+
+          <IoStatsChart
+            aria-label="Statistics"
+            onClick={() => navigate('/statistics')}
+            className="cursor-pointer"
+          />
+
         <div className="relative">
           <FaRegUserCircle
             onClick={handleOpen}
             className="cursor-pointer"
           />
 
-          {open && (
-            <ul className="absolute right-0 mt-2 w-40 bg-white border border-gray-200 rounded-md shadow-lg z-50">
-              <li>
-                <button className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100 text-xl">
-                  Change password
-                </button>
-              </li>
-              <li>
-                <button className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100 text-xl">
-                  Change profile picture
-                </button>
-              </li>
-            </ul>
-          )}
-        </div>
+        {open && (
+        <ul className="absolute right-0 mt-1 w-48  rounded shadow-lg z-50 ">
+            <li>
+            <button className="bg-cream  w-full text-left px-4 py-2 text-olive text-xl cursor-pointer "
+            >
+                Change password
+            </button>
+            </li>
+            <li>
+            <button className="bg-cream  w-full text-left px-4 py-2 text-olive text-xl cursor-pointer"
+            onClick={() => {
+              localStorage.removeItem('token');
+              navigate('/login');
+              }}
+            >
+                Logout
+            </button>
+            </li>
+        </ul>
+        )}
 
-        <IoLogOut
-          aria-label="Logout"
-          onClick={() => {
-            localStorage.removeItem('token');
-            navigate('/login');
-          }}
-          className="text-4xl cursor-pointer"
-        />
+        </div>
       </div>
     </div>
   );
