d92fcc6b7db8e6e6194aa206df6f0cd1fdc6596a
tomek-actions <tomek-actions@github.com>
2025-08-06 08:35:52 +0000

    chore: format code with Prettier

--- frontend/src/tests/Navbar.test.tsx
+++ frontend/src/tests/Navbar.test.tsx
@@ -54,9 +54,7 @@ describe('Navbar Component (withAuth)', () => {
         )
 
         await waitFor(() => {
-            expect(
-                screen.getByText(/Flashcards/i)
-            ).toBeInTheDocument()
+            expect(screen.getByText(/Flashcards/i)).toBeInTheDocument()
         })
     })
 
@@ -98,9 +96,7 @@ describe('Navbar Component (withAuth)', () => {
         )
 
         await waitFor(() => {
-            expect(
-                screen.getByText(/Flashcards/i)
-            ).toBeInTheDocument()
+            expect(screen.getByText(/Flashcards/i)).toBeInTheDocument()
         })
 
         const statsIcon = screen.getByLabelText('Statistics')
@@ -126,9 +122,7 @@ describe('Navbar Component (withAuth)', () => {
         )
 
         await waitFor(() => {
-            expect(
-                screen.getByText(/Flashcards/i)
-            ).toBeInTheDocument()
+            expect(screen.getByText(/Flashcards/i)).toBeInTheDocument()
         })
 
         const logoutIcon = screen.getByLabelText('Logout')
