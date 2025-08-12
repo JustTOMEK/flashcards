379abe2ba139dafe4421fd5b68be4efdd0d121a3
Tomaszek <trunio31@gmail.com>
2025-08-12 13:57:10 +0200

    changed the port number

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
-// vite.config.ts
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+        port: 2115,
+    },
     test: {
         globals: true,
         environment: 'jsdom',
